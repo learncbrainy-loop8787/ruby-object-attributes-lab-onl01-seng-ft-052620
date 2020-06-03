eb934eed67fd29cbbc97e189d8f6593e22ad9129
korey ko <koreykendrick1@gmail.com>
2020-06-03 03:56:23 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+require "pry"
+class Dog
+def name=(dog_name)
+    @name = dog_name
+ end
+  def name
+    @name
+  end
+end
+
+class Dog 
+def breed=(breed)
+    @breed = breed
+  
+end
+  def breed
+    @breed
+  end
+end
+
+
+
+
